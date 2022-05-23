2bd8e34ef6e71ee6763f9dabbd57fa0d3b206681
benjamindpb <benjamin.dpb@gmail.com>
2022-05-23 13:36:59 -0400

    Get tsv with url and coord

--- entity_analysis.py
+++ entity_analysis.py
@@ -5,21 +5,22 @@ BYTES = 10**8
 filename = 'latest-truthy.nt.gz'
 
 
-def get_entities_and_coord(bytes=-1, filename):
+def get_entities_and_coord(bytes=-1):
     start = time.time()
     n_entities = 0
-    with open(filename, 'r') as f, open('entities.tsv', 'w') as e:
+    with gzip.open('latest-truthy.nt.gz', 'r') as f, open('entities.tsv', 'w') as e:
         print('Reading file...')
         file_content = f.readlines(bytes)
         e.write('{}\t{}\t{}\n'.format('entity_url', 'latitude', 'longitude'))
         for line in file_content:
             str_line = line.decode()
-            if "P625" in str_line:
-                print(str_line)
+            if '<http://www.wikidata.org/prop/direct/P625>' in str_line: 
+                # print(str_line)
                 n_entities += 1
                 split = str_line.split(' ')
-                lat = split[2]
-                lon = split[3]
+                print(split)
+                lon = split[2].split('(')[1]
+                lat = split[3].split(')"^^')[0]
                 e.write('{}\t{}\t{}\n'.format(split[0], lat, lon))
     end = time.time()
     print('Time working: {}\n'.format(convert(end - start)))
@@ -28,4 +29,4 @@ def get_entities_and_coord(bytes=-1, filename):
 def convert(seconds):
     return time.strftime("%H:%M:%S", time.gmtime(seconds))
 
-get_entities_and_coord(BYTES, filename)
+get_entities_and_coord(BYTES)
